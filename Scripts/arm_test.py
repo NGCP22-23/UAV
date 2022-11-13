64c63de3695fcdbb1dda24930cc36a9c85f6a43f
ChongNelson <NelsonwChong01@gmail.com>
2022-11-12 16:05:25 -0800

    I fixed arm_test file

--- Scripts/arm_test.py
+++ Scripts/arm_test.py
@@ -2,6 +2,11 @@ import time
 import os
 import platform
 import sys
+
+if sys.version_info.major == 3 and sys.version_info.minor >= 10:
+    import collections
+    setattr(collections, "MutableMapping", collections.abc.MutableMapping)
+
 import collections
 import collections.abc
 
