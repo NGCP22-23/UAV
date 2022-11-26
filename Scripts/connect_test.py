0304f9a18a58a635c93610a1bf11f7ca8cd96e3c
Garrett Adams <78715643+garrettadamsss@users.noreply.github.com>
2022-11-26 01:40:45 -0800

    Update connect_test.py

--- Scripts/connect_test.py
+++ Scripts/connect_test.py
@@ -2,5 +2,7 @@ from dronekit import connect
 
 # Connect to UDP endpoint.
 vehicle = connect('/dev/ttyTHS1', wait_ready=True)
+#print firmware version
+print "Autopilot Firmware version: %s" % vehicle.version
 # Use returned Vehicle object to query device state - e.g. to get the mode:
 print("Mode: %s" % vehicle.mode.name)
