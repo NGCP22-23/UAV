74da1e1565ddedc481496f8491388f7a418bbb66
Garrett Adams <garrettadamssss@gmail.com>
2022-11-27 05:28:39 -0800

    m

--- Scripts/blah.py
+++ Scripts/blah.py
@@ -4,7 +4,7 @@ import time
 the_connection = mavutil.mavlink_connection('/dev/ttyTHS1', buad=57600)
 
 the_connection.wait_heartbeat()
-print("Heartbeat from system (system %u component %u)" % (the_connection.target_system, the_connection.target_component)
+print("Heartbeat from system (system %u component %u)" % (the_connection.target_system, the_connection.target_component))
 
 data_rate = 120 
 the_connection.mav.request_data_stream_send(the_connection.target_system, the_connection.target_components, mavutil.mavlink.MAV_DATA_STREAMALL, data_rate, 1)
