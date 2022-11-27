733dc7f3588afb4f82f9997c0bd0a960393965c5
Garrett Adams <garrettadamssss@gmail.com>
2022-11-26 20:13:28 -0800

    updating connection script

--- Scripts/connect_test.py
+++ Scripts/connect_test.py
@@ -1,8 +1,24 @@
 from dronekit import connect
 
-# Connect to UDP endpoint.
-vehicle = connect('/dev/ttyTHS1', wait_ready=True)
-#print firmware version
-print "Autopilot Firmware version: %s" % vehicle.version
-# Use returned Vehicle object to query device state - e.g. to get the mode:
-print("Mode: %s" % vehicle.mode.name)
+
+def ConnectToVehicle():
+    # Connect to UDP endpoint.
+    vehicle = connect('/dev/ttyTHS1',baud=57600,wait_ready=True)
+    return vehicle
+
+def ConnectionTests(vehicle):
+    #print firmware version
+    print('Autopilot version: %s'%vehicle.version)
+    # Use returned Vehicle object to query device state - e.g. to get the mode:
+    print("Mode: %s" % vehicle.mode.name)
+    # print mode
+    print ("Mode: %s" % vehicle.mode.name)
+    # print arming status
+    print ("Armed: %s" % vehicle.armed)
+    
+
+    
+plane = ConnectToVehicle()
+ConnectionTests(plane)
+
+
