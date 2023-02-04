2f2f85701f7b9281ce373f2cbbdc6e90802bb23e
Garrett Adams <garrettadamssss@gmail.com>
2023-02-04 15:16:27 -0800

    workign on demo

--- DroneKitScripts/demo.py
+++ DroneKitScripts/demo.py
@@ -1,9 +1,15 @@
+# 1. Connect Jetson and Pixhawk
+#   - ensure connection
+# 2. Change to
+# 
+
 import dronekit
 import socket
 import exceptions
 import Plane
 
-plane = Plane(conection_string = '/dev/ttyACM0')
-
+# plane = Plane(conection_string = '/dev/ttyACM0')
+plane = Plane(conection_string = 'tcp:127.0.0.1:5760')
+plane.connect()
 
 
