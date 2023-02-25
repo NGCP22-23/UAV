95fc822735801618b7f74b84868d3c4d9028d777
Garrett Adams <garrettadamssss@gmail.com>
2023-02-25 15:11:47 -0800

    added basic api-endpoint

---
+++ Communications/API.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import pandas as pd
+import ast
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class UAV(Resource):
+    def get(self):
+        return {'data': "poop"}
+
+api.add_resource(UAV, '/UAV')
+
+if __name__ == '__main__':
+    app.run
