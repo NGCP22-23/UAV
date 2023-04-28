b575882a39ab9cdc8e9f0d136f0554d75f3d13b6
Lex-ari <amarianocello@gmail.com>
2023-04-28 16:03:42 -0700

    Door works

--- Ext/PayloadSequence.py
+++ Ext/PayloadSequence.py
@@ -1,7 +1,7 @@
 import Jetson.GPIO as GPIO
 import time
 
-PIN_A = 38
+PIN_A = 33
 #DOOR = 32
 HERTZ_TO_MICROSECONDS = 1000000.0
 PIN_SERVO_IN_PWM = 2200.0 / HERTZ_TO_MICROSECONDS
@@ -11,12 +11,24 @@ DOOR_SERVO_PWM_CLOSED = 850.0 / HERTZ_TO_MICROSECONDS
 
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(PIN_A, GPIO.OUT)
+GPIO.setup(PIN_A, GPIO.OUT, initial=GPIO.HIGH)
 #GPIO.setup(DOOR, GPIO.OUT)
+pin_a = GPIO.PWM(PIN_A, 200)
+
+pin_a.start(0)
 
 print("starting")
-while True:
-    GPIO.PWM(PIN_A, PIN_SERVO_IN_PWM)
-    time.sleep(1)
-    GPIO.PWM(PIN_A, PIN_SERVO_OUT_PWM)
-    time.sleep(1)
+
+try:
+    while True:
+        pin_a.stop()
+        time.sleep(0.1)
+        pin_a.start(100)
+        time.sleep(0.1)
+        pin_a.stop()
+        time.sleep(0.1)
+        pin_a.start(0)
+        time.sleep(0.1)
+finally:
+    pin_a.stop()
+    GPIO.cleanup()
